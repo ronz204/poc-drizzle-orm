71aa97457b659f92e05d06af2243d3e19f2939fe
roney ruiz <roneyruizrojas@gmail.com>
2025-09-23 22:47:31 -0600

    chore: add active field to users schema

--- src/Schemas/Users.ts
+++ src/Schemas/Users.ts
@@ -1,8 +1,9 @@
-import { pgTable, varchar, serial } from "drizzle-orm/pg-core";
+import { pgTable, varchar, serial, boolean } from "drizzle-orm/pg-core";
 
 export const Users = pgTable("users", {
   id: serial("id").primaryKey().notNull(),
   name: varchar("name", { length: 256 }).notNull(),
+  active: boolean("active").notNull().default(true),
   password: varchar("password", { length: 256 }).notNull(),
 });
 
