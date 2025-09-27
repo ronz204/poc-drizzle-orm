5a0c6a960c44978679bd49ef7f3aa09bfc1f4d18
roney ruiz <roneyruizrojas@gmail.com>
2025-09-26 21:48:16 -0600

    feat: add specific params to concrete specs

--- src/Main.ts
+++ src/Main.ts
@@ -3,7 +3,7 @@ import { Drizzle } from "@Database/Drizzle";
 import { ExistingUserSpec } from "@Specs/Shared/ExistingUserSpec";
 
 const uow = new Unit(Drizzle);
-const spec = new ExistingUserSpec(1);
+const spec = new ExistingUserSpec({ id: 1 });
 
 const records = await uow.user.select(spec);
 console.log(records[0]?.name);
--- src/Specs/Shared/ExistingUserSpec.ts
+++ src/Specs/Shared/ExistingUserSpec.ts
@@ -2,10 +2,14 @@ import { eq } from "drizzle-orm";
 import { Users } from "@Database/Schemas/Users";
 import { UserSpecification } from "@Specs/Bases/UserSpecification";
 
+interface Params {
+  id: number;
+};
+
 export class ExistingUserSpec extends UserSpecification {
-  constructor(id: number) {
+  constructor(params: Params) {
     super();
     this.withPagination({ page: 1, limit: 1 });
-    this.withFiltering(eq(Users.id, id));
+    this.withFiltering(eq(Users.id, params.id));
   };
 };
